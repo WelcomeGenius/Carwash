ad352f70819c4bb2fbe3e7ca9a257ded818ac2f5
WelcomeGenius <152023222+WelcomeGenius@users.noreply.github.com>
2024-01-25 17:41:14 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+// document.querySelector(`.navbar-icon`).addEventListener(`click`, function (){
+//     document.querySelector(`header`).style.visibility = `visible`;
+// });
+
+// document.querySelector(`.fa-times`).addEventListener(`click`, function (){
+//     document.querySelector(`.fa-times`).style.visibility = `hidden`;
+//     document.querySelector(`header`).style.visibility = `hidden`;
+// });
+
+
+// document.querySelector(`.navbar-icon`).style.visibility =  `visible`;
